69de4d6ef1f298659e40ba48df9ceb61c98966e6
Arron <beale86@msn.com>
2023-03-25 14:46:14 +0000

    Add Mealitem component to AvailableMeals, change currency to euro, change dummy meal foods

--- beales-meals/src/components/Meals/AvailableMeals.js
+++ beales-meals/src/components/Meals/AvailableMeals.js
@@ -1,35 +1,43 @@
 import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 import classes from './AvailableMeals.module.css';
 
 const DUMMY_MEALS = [
   {
     id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
+    name: 'Snack Box',
+    description: 'Finest chips and fried chicken',
+    price: 7.99,
   },
   {
     id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
+    name: 'Spice Box',
+    description: 'A spicy box of chicken and chips!',
+    price: 6.99,
   },
   {
     id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
+    name: 'Quarter Pounder Meal',
+    description: 'Quarter pounder burger with chips + choice of drink',
+    price: 9.99,
   },
   {
     id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
+    name: 'Fish and Chips',
+    description: 'Battered cod with chips',
+    price: 6.99,
   },
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
 
   return (
     <section className={classes.meals}>
